8723a1a5a1665f6565257f74a16a20a98b1bd787
Alex Teper <ateper@fi.uba.ar>
2024-12-29 06:01:52 -0300

    feat: add database connection and schema for stories, include example .env file

--- apps/story-manager/app/page.tsx
+++ apps/story-manager/app/page.tsx
@@ -2,8 +2,22 @@
 
 import { Button } from "@workspace/ui/components/shadcn/button";
 import Link from "next/link";
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+import { stories } from "@/database/schema";
 
 export default async function Page() {
+  const connectionString = process.env.DATABASE_URL;
+  console.log(connectionString, "connection string");
+
+  // Disable prefetch as it is not supported for "Transaction" pool mode
+  const client = postgres(connectionString!, { prepare: false });
+  const db = drizzle(client);
+
+  const allStories = await db.select().from(stories);
+
+  console.log(allStories, "all stories");
+
   return (
     <div className="flex items-center justify-center ">
       <div className="flex flex-col items-center justify-center gap-4">
