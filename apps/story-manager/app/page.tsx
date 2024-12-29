22619cd5b24e144cc73069fead3207707b01389a
Alex Teper <ateper@fi.uba.ar>
2024-12-29 02:33:07 -0300

    feat: update Page component to use async function for improved data handling

--- apps/story-manager/app/page.tsx
+++ apps/story-manager/app/page.tsx
@@ -1,6 +1,8 @@
+"use server";
+
 import StoryDeckManager from "@/components/StoryDeckManager/StoryDeckManager";
 
-export default function Page() {
+export default async function Page() {
   return (
     <div className="flex items-center justify-center ">
       <div className="flex flex-col items-center justify-center gap-4">
