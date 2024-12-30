5db276d88b4a2d525a0e44a56112946d6c451ac3
Alex Teper <ateper@fi.uba.ar>
2024-12-30 07:20:59 -0300

    feat: wrap title in Navbar component with a link to the home page

--- packages/ui/src/components/layout/navbar.tsx
+++ packages/ui/src/components/layout/navbar.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { ModeToggle } from "../mode-toggle.client";
+import Link from "next/link";
 
 interface Props {
   title: string;
@@ -9,7 +10,9 @@ const Navbar: React.FC<Props> = ({ title }) => {
   return (
     <header className="w-full shadow">
       <nav className="max-w-6xl mx-auto flex items-center justify-between p-4">
-        <h1 className="text-xl font-bold">{title}</h1>
+        <Link href="/">
+          <h1 className="text-xl font-bold">{title}</h1>
+        </Link>
         <ModeToggle />
       </nav>
     </header>
